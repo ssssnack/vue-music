5c723b34e33666a4318ac9f922e04b956ac5df40
ssssnack <247770638@qq.com>
2018-03-01 17:06:02 +0800

    4-4

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,21 +1,27 @@
 /* eslint-disable */
+//封装jsonp
 import originJSONP from 'jsonp'
 
 export default function jsonp(url, data, option) {
+
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
   return new Promise((resolve, reject) => {
     originJSONP(url, option, (err, data) => {
-        if(!err){
-            resolve(data)
-        }else{
-            reject(err)
-        }
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
     })
   })
 }
 
 function param(data) {
-    let url =''
-    for(var k in data) {
-        let value =data[k] !== undefined 
-    }
+  let url = ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += `&${k}={encodeURIComponent(value)}`
+  }
+  return url ? url.substring(1) : ''
 }
